07fe008b0d34a0a6dcf66acf791ca42889f3b265
Thierry Champion <thierry.champion@polymtl.ca>
2023-10-20 10:44:53 -0400

    external memory correction

--- tp/tp7/lib/externalMemory.cpp
+++ tp/tp7/lib/externalMemory.cpp
@@ -5,16 +5,16 @@ ExternalMemory::ExternalMemory() :
 
 void ExternalMemory::write(const uint16_t addressStart, const char *input, uint8_t length)
 {
-    char writing[length + 1];
+    char writing[length];
 
     for (uint8_t i = 0; i < length; i++) {
         writing[i] = input[i];
     }
 
-    _protocolManager.ecriture(addressStart, (uint8_t*)writing, length + 1);
+    _protocolManager.ecriture(addressStart, (uint8_t*)writing, length);
 }
 
 void ExternalMemory::read(const uint16_t addressStart, char* output, uint8_t length)
 {
-    _protocolManager.lecture(addressStart, (uint8_t*)output, length + 1);
+    _protocolManager.lecture(addressStart, (uint8_t*)output, length);
 }
