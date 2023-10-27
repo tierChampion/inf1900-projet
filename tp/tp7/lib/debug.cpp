b2489b60d274c4e4d0233d5ac87b44ab5cc422ad
Nikolai Olekhnovitch <nikolai.olekhnovitch@polymtl.ca>
2023-10-27 10:57:23 -0400

    Implemented float type to debug class - Niko

--- tp/tp7/lib/debug.cpp
+++ tp/tp7/lib/debug.cpp
@@ -1,11 +1,43 @@
 #include <debug.h>
 #include <comm.h>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <math.h>
 
-// Overload fuction for various types (uint8_t, uint16_t, float, char)
 
-void display(uint8_t x) {
-    Comm comm = Comm();
+Comm comm;
+
+const uint16_t MAX_BUFFER_SIZE = 7;
+
+void display(uint8_t x)
+{
     char str[80];
-    sprintf(str, "Debug message: %i\n", x);
-    comm.transmitData((uint8_t*)str, strlen(str));
+    sprintf(str, "Debug message (uint8_t): %u\n", x);
+    Comm::transmitData((uint8_t *)str, strlen(str));
 }
+
+
+void display(uint16_t x)
+{
+    char str[80];
+    sprintf(str, "Debug message (uint16_t): %u\n", x);
+    Comm::transmitData((uint8_t *)str, strlen(str));
+}
+
+
+void display(float x)
+{
+    char buf[MAX_BUFFER_SIZE];
+    dtostre(x, buf, 2, 'e');
+    char str[80];
+    sprintf(str, "Debug message (float): %s\n", buf);
+    Comm::transmitData((uint8_t *)str, strlen(str));
+}
+
+void display(char *x)
+{
+    char str[80];
+    sprintf(str, "Debug message (char): %s\n", x);
+    Comm::transmitData((uint8_t *)str, strlen(str));
+}
+
