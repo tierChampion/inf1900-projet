a035628525003da3ce6b079d7370b549ce995d47
Thierry Champion <thierry.champion@polymtl.ca>
2023-11-05 23:34:58 -0500

    single makefile compilation

--- tp/tp9/build-debug.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-make clean -C ../lib
-make debug -C ../lib
-make clean
-make debug
-make install
--- tp/tp9/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-make clean -C ../lib
-make -C ../lib
-make clean
-make
-make install
