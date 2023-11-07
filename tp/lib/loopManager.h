4d470842c60a561c2483e2b89dc1e5f43809011f
Raissa Oumarou Petitot <raissa.oumarou-petitot@polymtl.ca>
2023-11-07 11:45:38 -0500

    loopManager Changed

--- tp/lib/loopManager.h
+++ tp/lib/loopManager.h
@@ -8,13 +8,16 @@ class LoopManager
 
 public:
     LoopManager();
-
-    void startLoop(uint16_t address, uint8_t counter);
-    uint16_t stopLoop(uint16_t address);
+    bool loopStarted() const;
+    bool loopEnded() const;
+    void startLoop(uint8_t index, uint8_t counter);
+    uint16_t stopLoop(uint8_t index);
 
 private:
     uint16_t _returnAddress;
     uint8_t _counter;
+    bool _loopStarted;
+    bool _loopEnded;
 };
 
 #endif
