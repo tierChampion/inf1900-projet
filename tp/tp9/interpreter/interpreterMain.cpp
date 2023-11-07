22ab4db7a4c53cc86655c8a95464309f91411eb4
Thierry Champion <thierry.champion@polymtl.ca>
2023-11-07 18:43:03 -0500

    interpreter cleanup

--- tp/tp9/interpreter/interpreterMain.cpp
+++ tp/tp9/interpreter/interpreterMain.cpp
@@ -3,20 +3,6 @@
 
 int main()
 {
-
-    // Led led(Port::A, PA0, PA1);
-    // led.setColor(LedColor::GREEN);
-    // _delay_ms(1000);
-    // led.setColor(LedColor::RED);
-    // _delay_ms(1000);
-    // led.setColor(LedColor::OFF);
-
     Interpreter interpreter = Interpreter();
-    interpreter.interpreteLoop();
-
-    // led.setColor(LedColor::GREEN);
-    // _delay_ms(1000);
-    // led.setColor(LedColor::RED);
-    // _delay_ms(1000);
-    // led.setColor(LedColor::OFF);
+    interpreter.interpretBytecode();
 }
