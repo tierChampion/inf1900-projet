e23a098893cb4b3b891d9e26f9c8d8d49897bd04
Thierry Champion <thierry.champion@polymtl.ca>
2023-11-20 10:32:26 -0500

    masterNavigation frame

--- projet/lib/master_navigation.cpp
+++ projet/lib/master_navigation.cpp
@@ -1,6 +1,67 @@
 #include "master_navigation.h"
 
 MasterNavigation::MasterNavigation() : _navigation(Navigation()),
-                                       _lineSensor(LineSensor())
+                                       _lineSensor(LineSensor()),
+                                       _distSensor(DistanceSensor()),
+                                       _mesureTimer(Timer1())
 {
+    // setting of the timer1
+
+    // what mode?
+    // what prescalar?
+    // which compare for the measures? (leave 1 for the flashing led)
+}
+
+void MasterNavigation::driveToIntersection()
+{
+    // drive forward while adjusting.
+
+    // check for intersections.
+
+    // if intersection, center on it and stop.
+}
+
+void MasterNavigation::driveOneUnit()
+{
+    // launch the measure timer for the needed time (to determine)
+
+    // drive forward while adjusting
+
+    // if time is done, stop
+
+    // stop the timer
+}
+
+void MasterNavigation::turnLeft()
+{
+    // pivot left
+
+    // check for a new line
+
+    // if centered on the new line stop
+}
+
+void MasterNavigation::turnRight()
+{
+    // pivot right
+
+    // check for a new line
+
+    // if centered on the new line stop
+}
+
+void MasterNavigation::turnMesuredRight()
+{
+
+    // launch the measure timer for the needed time (to determine)
+
+    // pivot right
+
+    // check for a new line
+
+    // if centered on the new line stop
+
+    // stop the timer and check the time measured
+
+    // determine if the angle is about 180
 }
--- projet/lib/master_navigation.h
+++ projet/lib/master_navigation.h
@@ -3,15 +3,30 @@
 
 #include "navigation.h"
 #include "line_sensor.h"
+#include "timer1.h"
+#include "distance_sensor.h"
 
 class MasterNavigation
 {
 public:
     MasterNavigation();
 
+    // forward until intersection
+    void driveToIntersection();
+    // forward for one unit (mesured)
+    void driveOneUnit();
+    // turn left
+    void turnLeft();
+    // turn right
+    void turnRight();
+    // turn right and mesure the angle (mesured)
+    void turnMesuredRight();
+
 private:
     Navigation _navigation;
     LineSensor _lineSensor;
+    DistanceSensor _distSensor;
+    Timer1 _mesureTimer;
 };
 
 #endif
